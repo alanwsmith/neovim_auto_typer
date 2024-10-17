606336b99e72ecc694be7b9b3c67007c3874fc52
Alan Smith <commit@alanwsmith.com>
2024-10-17 00:12:53 -0400

    Starting to read config file

--- auto_typer.lua
+++ auto_typer.lua
@@ -1,14 +1,53 @@
 local updates = {
   content = {},
   runner = {},
-  debug = 0
+  debug = 1
 }
+
+function string:split(delimiter)
+  local result = { }
+  local from  = 1
+  local delim_from, delim_to = string.find( self, delimiter, from  )
+  while delim_from do
+    table.insert( result, string.sub( self, from , delim_from-1 ) )
+    from  = delim_to + 1
+    delim_from, delim_to = string.find( self, delimiter, from  )
+  end
+  table.insert( result, string.sub( self, from  ) )
+  return result
+end
+
+updates.load_script = function() 
+  local fn = "/Users/alan/workshop/nvim_auto_typer/auto-type-script.txt"
+  local f = assert(io.open(fn, "r"))
+  local script_data = f:read("*all")
+  local lines = script_data:split("\n")
+  f:close()
+
+  for _, line in ipairs(lines) do
+    if line ~= "" then
+      local line_parts = line:split("|")
+      if line_parts[1] == "newline" then
+        print("GOT NEWLINE")
+      elseif line_parts[1] == "pause" then
+        print("GOT PAUSE")
+      elseif line_parts[1] == "tab" then
+        print("GOT TAB")
+      elseif line_parts[1] == "write" then
+        print("GOT WRITE")
+      end
+    end
+  end
+end
+
+
 
 updates.content[1] = { action = "write", data = "-- this is some code" }
 updates.content[2] = { action = "newline" }
 updates.content[3] = { action = "write", data = " and some more" }
 updates.content[4] = { action = "newline" }
 updates.content[5] = { action = "write", data = " and even more" }
+
 
 
 updates.deploy = function(d)
@@ -65,8 +104,9 @@ updates.go = function()
 end
 
 updates.run = function() 
-  updates.make_runner()
-  updates.go()
+  updates.load_script()
+  -- updates.make_runner()
+  -- updates.go()
 end
 
 updates.run()
