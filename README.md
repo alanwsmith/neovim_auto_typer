8b3d28c89dfc85191235393e740b3074a924a68b
Alan Smith <commit@alanwsmith.com>
2024-10-17 20:53:30 -0400

    Starting to clean up for first version

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
-# nvim_auto_typer
+-- title 
+
+Neovim Auto Typer
 
 A tool to type things into Neovim automatically
 in order to make screen recordings. 
 
+-- h2
 
+Commands
+
+-- list/
+
+- skip-start
+
+- skip-end
+
+-- /list
+
+
+
+
+-- metadata
+-- id: 2nauegoo
 
--- lua/auto_typer/init.lua
+++ lua/auto_typer/init.lua
@@ -1,57 +1,17 @@
 local M = {
+  source = os.getenv('HOME') .. "/Desktop/auto-typer-script.txt",
   content = {}
 }
 
--- not sure how naming spacing works here so 
--- prefixing with `aws_` 
--- NOTE: This feels like it's messing with string
--- globally. If that's the case, I want to find
--- another approach
-function string:aws_split(delimiter)
-  local result = {}
-  local from  = 1
-  local delim_from, delim_to = string.find( self, delimiter, from  )
-  while delim_from do
-    table.insert( result, string.sub( self, from , delim_from-1 ) )
-    from  = delim_to + 1
-    delim_from, delim_to = string.find( self, delimiter, from  )
-  end
-  table.insert( result, string.sub( self, from  ) )
-  return result
-end
-
-function M.run()
-  M.load_script()
-  vim.cmd('NvimTreeClose')
-  vim.cmd('set paste')
-  M.type_the_script()
-  vim.cmd('set nopaste')
-  vim.cmd('NvimTreeOpen')
-end
-
-function M.type_the_script() 
-  for _, v in ipairs(M.content) do
-    if v.action == "newline" then
-      vim.api.nvim_paste("\n", false, -1)
-    elseif v.action == "pause" then
-      vim.uv.sleep(1000)
-    elseif v.action == "tab" then
-      vim.api.nvim_paste("\t", false, -1)
-    elseif v.action == "write" then
-      M.output_chars(v.data)
-    end
-  end
-end
-
 function M.load_script() 
-  local fn = "/Users/alan/workshop/nvim_auto_typer/auto-type-script.txt"
-  local f = assert(io.open(fn, "r"))
+  M.content = {}
+  local f = assert(io.open(M.source, "r"))
   local script_data = f:read("*all")
-  local lines = script_data:split("\n")
+  local lines = M.split(script_data, "\n")
   f:close()
   for _, line in ipairs(lines) do
     if line ~= "" then
-      local line_parts = line:split("|")
+      local line_parts = M.split(line, "|")
       if line_parts[1] ~= nil then
         if line_parts[1] == "newline" then
           table.insert(M.content, { action = "newline" })
@@ -76,6 +36,40 @@ function M.output_chars(data)
   end
 end
 
--- M.run()
+function M.run()
+  M.load_script()
+  vim.cmd('NvimTreeClose')
+  vim.cmd('set paste')
+  M.type_the_script()
+  vim.cmd('set nopaste')
+  vim.cmd('NvimTreeOpen')
+end
+
+function M.split(str, delimiter) 
+  local results = {}
+  local from = 1
+  local delim_from, delim_to = string.find(str, delimiter, from)
+  while delim_from do
+    table.insert(results, string.sub(str, from , delim_from-1))
+    from  = delim_to + 1
+    delim_from, delim_to = string.find(str, delimiter, from)
+  end
+  table.insert(results, string.sub(str, from))
+  return results
+end
+
+function M.type_the_script() 
+  for _, v in ipairs(M.content) do
+    if v.action == "newline" then
+      vim.api.nvim_paste("\n", false, -1)
+    elseif v.action == "pause" then
+      vim.uv.sleep(1000)
+    elseif v.action == "tab" then
+      vim.api.nvim_paste("\t", false, -1)
+    elseif v.action == "write" then
+      M.output_chars(v.data)
+    end
+  end
+end
 
 return M
