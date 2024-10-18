bba502058a1028e966f90ec22dc17ff677d100d7
Alan Smith <commit@alanwsmith.com>
2024-10-18 00:51:31 -0400

    Added stop and working on grimoire code showing

--- lua/auto_typer/init.lua
+++ lua/auto_typer/init.lua
@@ -1,8 +1,9 @@
 local M = {
-  debug = "off",
-  source = os.getenv('HOME') .. "/Desktop/auto-typer-script.txt",
+  stop = false,
   content = {},
+  debug = "off",
   popup_id = nil,
+  source = os.getenv('HOME') .. "/Desktop/auto-typer-script.txt",
 }
 
 function M.close_popup()
@@ -34,16 +35,18 @@ function M.load_script()
     if line ~= "" then
       local line_parts = M.split(line, "|")
       if line_parts[1] ~= nil then
-        if line_parts[1] == "close-popup" then
+        if line_parts[1] == "stop" then
+          table.insert(M.content, { action = "stop" })
+        elseif line_parts[1] == "close-popup" then
           table.insert(M.content, { action = "close-popup" })
         elseif line_parts[1] == "cmd" then
           table.insert(M.content, { action = "cmd", command = M.trim(line_parts[2]) })
+        elseif line_parts[1] == "combo" then
+          table.insert(M.content, { action = "combo", keys = M.trim(line_parts[2]) })
         elseif line_parts[1] == "debug" then
           table.insert(M.content, { action = "debug", state = M.trim(line_parts[2]) })
         elseif line_parts[1] == "end-skip" then
           table.insert(M.content, { action = "end-skip" })
-        -- elseif line_parts[1] == "feedkeys" then
-          --table.insert(M.content, { action = "feedkeys", keys = M.trim(line_parts[2]) })
           -- setting the file extension isn't working
           -- as expected this way, so just opening a tmp file
           -- for now
@@ -51,6 +54,8 @@ function M.load_script()
         --   table.insert(M.content, { action = "filetype", extension = M.trim(line_parts[2]) })
         elseif line_parts[1] == "newline" then
           table.insert(M.content, { action = "newline" })
+        elseif line_parts[1] == "normal" then
+          table.insert(M.content, { action = "normal", data = M.trim(line_parts[2]) })
         elseif line_parts[1] == "pause" then
           table.insert(M.content, { action = "pause", kind = M.trim(line_parts[2]) })
         elseif line_parts[1] == "open_file" then
@@ -86,6 +91,9 @@ function M.open_popup()
   local floating_buffer = vim.api.nvim_create_buf(false, true)
   local floating_window = vim.api.nvim_open_win(floating_buffer, true, opts)
   M.popup_id = floating_window
+  -- nvim_paste hack to make sure the window shows up
+  vim.api.nvim_paste("", false, -1)
+  M.do_delay(350, 350)
 end
 
 function M.output_chars(data)
@@ -129,33 +137,38 @@ function M.trim(str)
 end
 
 function M.type_the_script() 
-  for _, v in ipairs(M.content) do
-    if v.action == "close-popup" then
-      M.close_popup()
-    elseif v.action == "cmd" then
-      vim.cmd(v.command)
-    elseif v.action == "debug" then
-      print("debug now: " .. v.state)
-      M.debug = v.state
---    elseif v.action == "feedkeys" then  
- --     local key = nvim_replace_termcodes(v.keys, v:true, v:false, v:true)
-  --    nvim_feedkeys(key, 'n', v:false)
-    -- turn this back on when filetype setting
-    -- is fixed
-    -- elseif v.action == "filetype" then
-    --   M.set_filetype(v.extension)
-    elseif v.action == "newline" then
-      vim.api.nvim_paste("\n", false, -1)
-    elseif v.action == "open-popup" then
-      M.open_popup()
-    elseif v.action == "open_file" then
-      M.open_file(v.path)
-    elseif v.action == "pause" then
-      M.do_delay(2000, 2000)
-    elseif v.action == "tab" then
-      vim.api.nvim_paste("\t", false, -1)
-    elseif v.action == "write" then
-      M.output_chars(v.data)
+  for _, val in ipairs(M.content) do
+    if M.stop == false then
+      if val.action == "stop" then
+        M.stop = true
+      elseif val.action == "close-popup" then
+        M.close_popup()
+      elseif val.action == "cmd" then
+        vim.cmd(val.command)
+      elseif val.action == "combo" then  
+        local keys = vim.api.nvim_replace_termcodes(val.keys, true, false, true)
+        vim.api.nvim_feedkeys(keys, 'm', true)
+      elseif val.action == "debug" then
+        M.debug = val.state
+      -- turn this back on when filetype setting
+      -- is fixed
+      -- elseif v.action == "filetype" then
+      --   M.set_filetype(v.extension)
+      elseif val.action == "newline" then
+        vim.api.nvim_paste("\n", false, -1)
+      elseif val.action == "normal" then
+        vim.cmd("normal " .. val.data)
+      elseif val.action == "open-popup" then
+        M.open_popup()
+      elseif val.action == "open_file" then
+        M.open_file(val.path)
+      elseif val.action == "pause" then
+        M.do_delay(2000, 2000)
+      elseif val.action == "tab" then
+        vim.api.nvim_paste("\t", false, -1)
+      elseif val.action == "write" then
+        M.output_chars(val.data)
+      end
     end
   end
 end
